ea3d9121fda49a6d4e0477e7035a65a4c0b77ad3
Salley <juxsalley@gmail.com>
2021-03-01 22:09:33 +0000

    Update post.rb
    remove user_id presence as required by TSE

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 10, maximum: 50 }
   validates :body, presence: true, length: { minimum: 10 }
-  validates :user_id, presence: true
   belongs_to :user
   has_many :comments
 end
