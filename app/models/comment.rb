4b06fffef9f8fb84406a9f6477a753248aaef70a
Salley <juxsalley@gmail.com>
2021-03-01 22:11:02 +0000

    Update comment.rb
    removed user_id and post presence validation as required by TSE

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,5 @@
 class Comment < ApplicationRecord
   validates :text, length: { minimum: 1 }, presence: true
-  validates :post_id, presence: true
-  validates :user_id, presence: true
   belongs_to :user
   belongs_to :post
 end
