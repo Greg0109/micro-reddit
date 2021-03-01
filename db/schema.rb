f149e49d77bc58cdfb0ce3e734576bd059a1a954
Greg0109 <greg.rabago@gmail.com>
2021-03-01 13:45:09 +0100

    Link comments to user and post

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_103203) do
+ActiveRecord::Schema.define(version: 2021_03_01_123725) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
     t.string "publish_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "post_id"
+    t.string "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
