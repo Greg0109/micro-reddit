bdf09fdd393bc57ff0d710596a54958e54704e56
juxsalley <juxsalley@gmail.com>
2021-03-01 12:12:16 +0000

    user : post association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     validates :username, length: { minimum: 5 }, presence: true
     validates :email, presence: true, length: { minimum: 8 }
-    has_many: posts
-    belongs_to :user, class_name: "user", foreign_key: "user_id"
-    def posts
-    end
+    has_many :posts
+    # belongs_to :user, class_name: "user", foreign_key: "user_id"
+    # def posts
+    # end
 end
