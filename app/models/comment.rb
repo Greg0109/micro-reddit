a0995babbe98f0ad828b9b5bc8136caf04d6e4d3
Greg0109 <greg.rabago@gmail.com>
2021-03-01 14:29:29 +0100

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
-    validates :text, length: { minimum: 1 }, presence: true
-    validates :post_id, presence: true
-    validates :user_id, presence: true
-    belongs_to :user
-    belongs_to :post
+  validates :text, length: { minimum: 1 }, presence: true
+  validates :post_id, presence: true
+  validates :user_id, presence: true
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    validates :username, length: { minimum: 5 }, presence: true
-    validates :email, presence: true, length: { minimum: 8 }
-    has_many :posts
-    has_many :comments
-    # belongs_to :user, class_name: "user", foreign_key: "user_id"
-    # def posts
-    # end
+  validates :username, length: { minimum: 5 }, presence: true
+  validates :email, presence: true, length: { minimum: 8 }
+  has_many :posts
+  has_many :comments
+  # belongs_to :user, class_name: "user", foreign_key: "user_id"
+  # def posts
+  # end
 end
